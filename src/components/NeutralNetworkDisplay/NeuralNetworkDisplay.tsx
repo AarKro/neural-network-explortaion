5dd01b9ca14283780dc562084b3f7eef9ee5241b
Aaron Kromer <kromer.aaron@gmail.com>
2024-12-15 16:49:06 +0100

    update results display

--- src/components/NeutralNetworkDisplay/NeuralNetworkDisplay.tsx
+++ src/components/NeutralNetworkDisplay/NeuralNetworkDisplay.tsx
@@ -1,23 +1,49 @@
 import { useAtom } from "jotai";
 import { FC, useMemo } from "react";
 import { networkOutputAtom } from "../../jotaiStore";
+import "./NeuralNetworkDisplay.css";
 
 export const NeuralNetworkDisplay: FC = () => {
   const [ networkOutput ] = useAtom(networkOutputAtom);
 
-  const sortedPredictions = useMemo(() => (
+  const predictions = useMemo(() => (
     Object.keys(networkOutput)
       .map((key) => ({ key, value: networkOutput[key] }))
-      .sort((a, b) => b.value - a.value)
   ), [networkOutput]);
 
+  const sortedPredictionsByScore = useMemo(() => (
+    [...predictions].sort((a, b) => b.value - a.value)
+  ), [predictions]);
+
+  const sortedPredictionsByKey = useMemo(() => (
+    [...predictions].sort((a, b) => a.key.localeCompare(b.key))
+  ), [predictions]);
+
   return (
-    <div>
-      {sortedPredictions.slice(0, 3).map((prediction) => (
-        <div key={prediction.key}>
-          {prediction.key}: {(prediction.value * 100).toFixed(2)}%
+    <div className="results">
+      <div>
+        <h4>Top Matches</h4>
+        {sortedPredictionsByScore.slice(0, 3).map((prediction) => (
+          <div key={prediction.key}>
+            {prediction.key}: {(prediction.value * 100).toFixed(2)}%
+          </div>
+        ))}
+      </div>
+      <div>
+        <h4>Predictions Table</h4>
+        <div>
+          <table>
+            <tbody>
+              {sortedPredictionsByKey.map((prediction) => (
+                <tr key={prediction.key}>
+                  <td>{prediction.key}</td>
+                  <td>{(prediction.value * 100).toFixed(2)}%</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
         </div>
-      ))}
+      </div>
       {/* <br/>
       <br/>
       <div dangerouslySetInnerHTML={{__html: getSvgOfNetwork()}}></div> */}
